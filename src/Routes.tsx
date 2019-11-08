46ec79d838d9dad3873d0c8fba4cb8c29429a126
maeda <example@email>
2019-11-08 17:59:32 +0900

    routers improve

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,13 +1,14 @@
 import * as React from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Counter from 'containers/counter/Container'
+import {Header} from 'views/Header'
 
 export function Routes() {
     return(
         <Router>
         <Switch>
-            <Route path='/' component={Counter} />
-            <Route path='/counter' component={Counter} />
+            <Route path='/' exact component={Header} />
+            <Route path='/counter' exact component={Counter} />
         </Switch>
         </Router>
     )
